81a7952e942ac84dbb28bd5764ea60122cb0ded9
Henrik Engström <me@henrikengstrom.com>
2025-11-29 09:11:22 +0100

    FiscalYear reference solved in models.

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,8 +2,15 @@ from app.models.account import Account
 from app.models.verification import Verification, TransactionLine
 from app.models.company import Company
 from app.models.customer import Customer, Supplier
-from app.models.invoice import Invoice, InvoiceLine, SupplierInvoice, SupplierInvoiceLine
+from app.models.invoice import (
+    Invoice,
+    InvoiceLine,
+    SupplierInvoice,
+    SupplierInvoiceLine,
+)
 from app.models.default_account import DefaultAccount
+from app.models.fiscal_year import FiscalYear
+from app.models.expense import Expense
 
 __all__ = [
     "Account",
@@ -17,4 +24,6 @@ __all__ = [
     "SupplierInvoice",
     "SupplierInvoiceLine",
     "DefaultAccount",
+    "FiscalYear",
+    "Expense",
 ]
